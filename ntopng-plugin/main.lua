27f89708c60ba20570be7b92fd6a03bbe102727b
drewp@bigasterisk.com <drewp@bigasterisk.com>
2021-07-16 01:03:46 -0700

    update lua; move to subdir

--- ntopng-plugin/main.lua
+++ ntopng-plugin/main.lua
@@ -1,4 +1,3 @@
--- this is an old copy from /opt/ntop-plugins/lanscape/main.lua
 
 local dirs = ntop.getDirs()
 package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
@@ -7,10 +6,15 @@ require "lua_utils"
 
 sendHTTPContentTypeHeader('text/plain')
 
+function string.starts(String,Start)
+   return string.sub(String,1,string.len(Start))==Start
+end
+
 for _, ifname in ipairs({"enp1s0", "ens5", "ens4"}) do
    interface.select(ifname)
 
    -- or interface.getHostsInfo ?
+   local ipver_filter = 4
    local hosts_stats = interface.getLocalHostsInfo(
       false,
       "column_mac", --sort
@@ -18,32 +22,30 @@ for _, ifname in ipairs({"enp1s0", "ens5", "ens4"}) do
       0, --skip
       true, --asc
       -- the rest are filters; see https://www.ntop.org/guides/ntopng/api/lua_c/interface/interface_hosts.html?highlight=getlocalhostsinfo#_CPPv412getHostsInfob6stringiib6string6stringiii6stringiiiibbbbb6string
-      nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, false, false, false, false, false)
+      nil, nil, nil, nil, nil, nil, nil, ipver_filter, nil, nil, false, false, false, false, false)
 
    print('hosts_count{ifname="'..ifname..'"} '..hosts_stats["numHosts"].."\n")
 
    for key, value in pairs(hosts_stats.hosts) do
-      local mac = value.mac
+      local mac = value.mac:lower()
+
+      -- ntop gives some hosts from other interfaces, I don't know why. Filed https://github.com/ntop/ntopng/issues/5633
+      if ifname == "enp1s0" and not value["ip"]:starts("10.1.") then goto continue end
+      if ifname == "ens5" and not value["ip"]:starts("10.2.") then goto continue end
+      if ifname == "ens4" and not value["ip"]:starts("192.168.") then goto continue end
+
 
       function printMetric(name, value)
-         print(name..'{iface="'..ifname..'",mac="'..mac..'"} '..value.."\n")
+         print(string.format('%s{iface="%s",mac="%s"} %f\n', name, ifname, mac, value))
       end
 
-      printMetric("host_ip", value.ip)
-      printMetric("host_ip_label", ip2label(value["ip"]))
-
+      local info_label = ""
       if(value["ip"] ~= nil) then
-         local label = hostinfo2label(value)
-
-         if label ~= value["ip"] then
-            printMetric("host_info_label", label)
-         end
+         info_label = hostinfo2label(value)
       end
 
-      if(value["name"] == nil) then
-         local hinfo = hostkey2hostinfo(key)
-         printMetric("host_label", hostinfo2label(hinfo))
-      end
+      print(string.format('host_names{iface="%s",mac="%s",ip="%s",label="%s",info_label="%s"} 1.0\n',
+                          ifname, mac, value.ip, ip2label(value.ip), info_label))
 
       printMetric("host_os", value.os)
 
@@ -63,6 +65,6 @@ for _, ifname in ipairs({"enp1s0", "ens5", "ens4"}) do
       printMetric("host_bytes_received", value["bytes.rcvd"])
 
       print("\n")
+      ::continue::
    end
 end
-
---
+++ ntopng-plugin/manifest.lua
@@ -0,0 +1,6 @@
+return {
+    title = "lanscape data dump",
+    description = "",
+    author = "drewp",
+    dependencies = {}
+}
